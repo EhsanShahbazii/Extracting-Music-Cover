305dc5f8a5615333aada0cac429be684b822c410
Ehsan Shahbazi <ehsan.shahbazicode@gmail.com>
2023-01-24 11:55:38 +0330

    add anime

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -1,1 +1,9 @@
-const $ = document;
+$.addEventListener("scroll", () => {
+  let scrollTop =
+    window.pageYOffset !== undefined
+      ? window.pageYOffset
+      : (document.documentElement || document.body.parentNode || document.body)
+          .scrollTop;
+
+  let scrolled = Math.floor(scrollTop / 10);
+});
